b9c8ade2df4f590916f2fc14e2ac89e7a5702a8e
Jake Browning <jake.reid.browning@gmail.com>
2017-07-19 11:21:19 +0100

    pong

---
+++ examples/games/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo ../../links --config=../../config -m -d --path=.:../ breakout.links
--- vdom-examples/run.sh
+++ vdom-examples/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo ../links --config=../config -m -d --path=.:../ mousetest.links
+sudo ../links --config=../config -m -d --path=.:../ pong.links
